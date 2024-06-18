a9c9761353a4164f5f0c333cb5862b442ceab422
Yasmine-Jamal <jmalyasmyn97@gmail.com>
2024-06-18 04:57:27 +0300

    modifi the navbar and make it responsive , modifi the buttons style ,and make links in navbar links the sections in home page below

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const bars_btn=document.querySelector('.toggle-btn');
+const bars_icon=document.querySelector('.toggle-btn i');
+const dropMenue=document.querySelector('.dropdown');
+
+bars_btn.onclick=function(){
+    dropMenue.classList.toggle('open');
+    const isOpen=dropMenue.classList.contains('open')
+
+    bars_icon.classList= isOpen ?'fas fa-times':'fas fa-bars';
+}
+/* animation sections */
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('.animated-section');
+
+    const sectionObserver = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target); // Stop observing once it's visible
+            }
+        });
+    }, { threshold: 0.2 }); // Trigger when 10% of the section is in view
+
+    sections.forEach(section => {
+        sectionObserver.observe(section);
+    });
+});
